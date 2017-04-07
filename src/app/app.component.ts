ecf8765b9c84c2e99aa0cf167019ce074d348ec1
jmllegrand <jml.legrand@gmail.com>
2017-04-07 19:46:51 +0200

    feat(products): property binding on src element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,11 @@ import {Component} from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
+    <img src="{{imageUrl}}">
     <jml-products></jml-products>
   `
 })
 export class AppComponent {
   title: string = 'My first Angular app!';
+  imageUrl: string = "https://www.w3schools.com/html/pic_mountain.jpg";
 }
