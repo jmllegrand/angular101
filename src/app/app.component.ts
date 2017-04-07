0aac93d3723d586e2fe8b3ed7b8830e88e4eedf8
jmllegrand <jml.legrand@gmail.com>
2017-04-07 20:50:52 +0200

    feat(products): event binding to the button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {Component} from '@angular/core';
     <img src="{{imageUrl}}">
     <jml-products></jml-products>
     <button class="btn btn-primary" [class.disabled]="!isValid">Submit</button>
+    <button (click)="onClickMe($event)">Toggle</button>
 
   `
 })
@@ -14,5 +15,10 @@ export class AppComponent {
   title: string = 'My first Angular app!';
   imageUrl: string = "https://www.w3schools.com/html/pic_mountain.jpg";
   isValid: boolean = false;
+
+  onClickMe($event: any) {
+    console.log("an event has been fired", $event);
+    this.isValid = !this.isValid;
+  }
 
 }
