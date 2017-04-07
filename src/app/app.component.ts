98f7e92df7e562b7b445fc3e004904a85bb9b516
jmllegrand <jml.legrand@gmail.com>
2017-04-07 19:48:13 +0200

    feat(products): css class binding on button element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,13 @@ import {Component} from '@angular/core';
     <h1>{{title}}</h1>
     <img src="{{imageUrl}}">
     <jml-products></jml-products>
+    <button class="btn btn-primary" [class.disabled]="!isValid">Submit</button>
+
   `
 })
 export class AppComponent {
   title: string = 'My first Angular app!';
   imageUrl: string = "https://www.w3schools.com/html/pic_mountain.jpg";
+  isValid: boolean = false;
+
 }
